3ffad88c4b3f432c65e085062c92537db78ecb4c
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-02-21 18:11:47 -0500

    added error handling"

--- backend/app.js
+++ backend/app.js
@@ -44,4 +44,24 @@ app.use(
 app.use(routes)
 
 
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
+
+
 module.exports = app;
