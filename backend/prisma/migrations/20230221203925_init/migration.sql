e620ba8749de86c0d01634c31c64a783aeb1e21a
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-02-21 17:18:34 -0500

    ok

---
+++ backend/prisma/migrations/20230221203925_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "hashed_password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
