c32b4ef311839bbd6327b04f8ac8233935e2d4ab
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-02-21 23:52:17 -0500

    feature done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
+
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
+
 
 router.get(
     '/',
@@ -18,6 +32,7 @@ router.get(
 
 router.post(
     '/',
+    validateLogin,
     async (req, res, next) => {
         const { credential, password } = req.body;
 
