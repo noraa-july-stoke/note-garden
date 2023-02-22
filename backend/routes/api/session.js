e562a643927cbb61ea166a66291bac50c41ea7df
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-02-21 23:39:44 -0500

    feature done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,6 +3,18 @@ const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
 
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({ user: null });
+    }
+);
 
 router.post(
     '/',
