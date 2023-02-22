42ecbc5f69ca36f940aae902338a4cbe55c1755f
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-02-21 22:43:43 -0500

    add api router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+
+
+
+router.use('/api', apiRouter);
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
@@ -9,7 +15,5 @@ router.get("/api/csrf/restore", (req, res) => {
         'XSRF-Token': csrfToken
     });
 });
-
-
 
 module.exports = router;
