078095e7b3f650f42f71242b974a0c0d601697c0
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-03-02 19:00:01 -0500

    ok

--- frontend/src/components/Buttons/DeleteButton/DeleteButton.jsx
+++ frontend/src/components/Buttons/DeleteButton/DeleteButton.jsx
@@ -4,10 +4,11 @@ import "./DeleteButton.css";
 
 //reusable delete button for deleting anything out of my database
 const DeleteButton = ({type, deletedState, id}) => {
-    const {isDeleted, setIsDeleted } = deletedState;
+    const { _ , setIsDeleted } = deletedState;
     const {deleteText, className, onClick} = DeleteReducer(type, setIsDeleted, id);
     useEffect(() => {
     }, [onClick])
     return <button className={className} onClick={onClick}>{deleteText}</button>
 }
+
 export default DeleteButton;
