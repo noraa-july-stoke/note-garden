f87fcd8c83686c7d9decc7e1bded2263ab236650
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-03-03 13:13:15 -0500

    ok

--- frontend/src/components/UserNotebooks/UserNotebooks.jsx
+++ frontend/src/components/UserNotebooks/UserNotebooks.jsx
@@ -26,7 +26,7 @@ const UserNotebooks = () => {
             <div className="side-panel-container">
                 <div className="tabs-container">
                     {Object.values(notebooks).map(notebook => {
-                        const colors = ['#FFC300', '#F7931E', '#FF5733', '#C70039', '#900C3F', 'sky blue', 'bubblegum pink', 'hot pink', 'cyan', 'lavender', 'lime green', 'eccentric blue', 'charcoal', 'pink', 'crimson', 'pale goldenrod', 'goldenrod', 'turquoise', 'jade', 'teal', 'aquamarine', '#581845', '#006266', '#009432', '#1B1464', '#833471', '#BB2B1F', '#ED4C67', '#0066CC', '#27AE60', '#E67E22', '#D35400', '#8E44AD', '#2C3E50', '#34495E', '#7F8C8D'];
+                        const colors = ['#FFC300', '#F7931E', '#FF5733', '#C70039', '#900C3F', '#87CEEB', '#FF69B4', '#FF007F', '#00FFFF', '#E6E6FA', '#32CD32', '#2E8B57', '#36454F', '#FFC0CB', '#DC143C', '#EEE8AA', '#DAA520', '#40E0D0', '#00A86B', '#008080', '#581845', '#006266', '#009432', '#1B1464', '#833471', '#BB2B1F', '#ED4C67', '#0066CC', '#27AE60', '#E67E22', '#D35400', '#8E44AD', '#2C3E50', '#34495E', '#7F8C8D'];
                         const randomColor = colors[Math.floor(Math.random() * colors.length)];
                         return (
                             <div
