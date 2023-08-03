9d61670eb6f1716160420598c7c4c34bb66b63a0
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-08-02 20:05:41 -0400

    update

--- backend/db/migrations/20230324215455-create-audio.js
+++ backend/db/migrations/20230324215455-create-audio.js
@@ -58,7 +58,7 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable(options, options);
